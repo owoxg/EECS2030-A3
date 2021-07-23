f5a591e46ee891e8ff31ffc8418eb4743353861e
Dexuan Ren <81537377+KenRen98@users.noreply.github.com>
2021-07-23 19:22:08 -0400

    Change on Main API APP

--- MLS/src/main/java/org/eecs/a3/teamafk/MLS/MlsApplication.java
+++ MLS/src/main/java/org/eecs/a3/teamafk/MLS/MlsApplication.java
@@ -5,13 +5,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.sql.*;
 
 @Controller
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MlsApplication {
 
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://mrleavesbbs.com:3306/mls";
+	static final String USER = "";
+	static final String PASS = "";
+
 	public static void main(String[] args) {
 		SpringApplication.run(MlsApplication.class, args);
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			Class.forName(JDBC_DRIVER);
+			System.out.println("连接数据库...");
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			System.out.println("连接成功！");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
 	}
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
@@ -21,4 +39,14 @@ public class MlsApplication {
 		return "Hello " + name + "!";
 	}
 	//http://localhost:9000/hello?name=TeamAFK
+
+	/*@RequestMapping(value = "/mlslookup", method = RequestMethod.GET)
+	public @ResponseBody
+	String mlsLookup(@RequestParam(value = "uuid", required = true) UUID uuid) {
+
+		return "1";
+	}
+	*/
 }
+
+
