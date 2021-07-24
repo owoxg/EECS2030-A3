5601c93d84c0d08208781011c8859b672b48449c
Dexuan Ren <81537377+KenRen98@users.noreply.github.com>
2021-07-24 03:51:35 -0400

    added first demo function

--- MLS/src/main/java/org/eecs/a3/teamafk/MLS/MlsApplication.java
+++ MLS/src/main/java/org/eecs/a3/teamafk/MLS/MlsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.sql.*;
+import java.util.Date;
 
 @Controller
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -13,18 +14,21 @@ public class MlsApplication {
 
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://mrleavesbbs.com:3306/mls";
-	static final String USER = "";
-	static final String PASS = "";
+	static final String USER = "mls";
+	static final String PASS = "mls123456";
+
+	Connection conn = null;
+	Statement stmt = null;
+	String sql;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MlsApplication.class, args);
 		Connection conn = null;
-		Statement stmt = null;
 		try {
 			Class.forName(JDBC_DRIVER);
-			System.out.println("连接数据库...");
+			System.out.println("Connecting...");
 			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-			System.out.println("连接成功！");
+			System.out.println("Succeeded！");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException throwables) {
@@ -47,6 +51,53 @@ public class MlsApplication {
 		return "1";
 	}
 	*/
+
+	@RequestMapping(value = "/building", method = RequestMethod.GET)
+	public @ResponseBody
+	String mlsLookup(@RequestParam(value = "id", required = true) int id) {
+		try {
+			try {
+				Class.forName(JDBC_DRIVER);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Connecting...");
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			System.out.println("Succeeded！");
+			stmt = conn.createStatement();
+			sql = "SELECT * FROM building WHERE id="+id;
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				String type = rs.getString("type");
+				int size = rs.getInt("size");
+				String address = rs.getString("address");
+				String country = rs.getString("country");
+				String schooldistrict = rs.getString("schooldistrict");
+				int bedrooms = rs.getInt("bedrooms");
+				int livingroom = rs.getInt("livingrooms");
+				int totalrooms = rs.getInt("totalrooms");
+				String expenses = rs.getString("expenses");
+				String builtdate = rs.getDate("builtdate").toString();
+				int floorlevel = rs.getInt("floorlevel");;
+				boolean pool = rs.getBoolean("pool");
+				boolean cableready = rs.getBoolean("cableready");
+				boolean furnished = rs.getBoolean("furnished");
+				boolean hassecurity = rs.getBoolean("hassecurity");
+				boolean hasgarden = rs.getBoolean("hasgarden");
+				int gardensize = rs.getInt("gardensize");
+				String description = rs.getString("description");
+				switch (type){
+					case "Condo":
+						BuildCondo condo = new Building.BuildingBuilder().type(type).size(size).address(address).country(country).schooldistrict(schooldistrict).bedrooms(bedrooms).livingroom(livingroom).totalrooms(totalrooms).expenses(expenses).builtdate(builtdate).floorlevel(floorlevel).pool(pool).cableready(cableready).furnished(furnished).hassecurity(hassecurity).hasgarden(hasgarden).gardensize(gardensize).description(description).buildcondo();
+						return condo.Display();
+				}
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+		return "Probably, this is a Bug";
+	}
+
 }
 
 
