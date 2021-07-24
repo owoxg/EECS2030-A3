51de58698328034e139951924ce5d67385454462
JinshanHan1 <87878287+JinshanHan1@users.noreply.github.com>
2021-07-24 13:14:13 -0700

    Update Building.java

--- MLS/src/main/java/org/eecs/a3/teamafk/MLS/Building.java
+++ MLS/src/main/java/org/eecs/a3/teamafk/MLS/Building.java
@@ -481,5 +481,25 @@ public abstract class Building implements MLSInterface {
         public BuildCondo buildcondo() {
             return new BuildCondo(size, address, country, schooldistrict, bedrooms, livingroom, totalrooms, expenses, builtdate, floorlevel, pool, cableready, furnished, hassecurity, hasgarden, gardensize, description);
         }
+        
+        public Detached detached() {
+            return new Detached(size, address, country, schooldistrict, bedrooms, livingroom, totalrooms, expenses, builtdate, floorlevel, pool, cableready, furnished, hassecurity, hasgarden, gardensize, description);
+        }
+        
+        public Land land() {
+            return new Land(size, address, country, schooldistrict, bedrooms, livingroom, totalrooms, expenses, builtdate, floorlevel, pool, cableready, furnished, gardensize, description);
+        }
+        
+        public Mobeile_Floating mobeile_floating() {
+            return new Mobeile_Floating(size, address, country, schooldistrict, bedrooms, livingroom, totalrooms, expenses, builtdate, floorlevel, pool, cableready, furnished, hassecurity, hasgarden, gardensize, description);
+        }
+        
+         public Semi_Detached semi_detached() {
+            return new Semi_Detached(size, address, country, schooldistrict, bedrooms, livingroom, totalrooms, expenses, builtdate, floorlevel, pool, cableready, furnished, hassecurity, hasgarden, gardensize, description);
+        }
+        
+         public Townhouse townhouse() {
+            return new townhouse(size, address, country, schooldistrict, bedrooms, livingroom, totalrooms, expenses, builtdate, floorlevel, cableready, furnished, hassecurity, hasgarden, gardensize, description);
+        }
     }
 }
